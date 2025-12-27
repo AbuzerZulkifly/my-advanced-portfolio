c0e6507c59b68d6a997180498ef6f1e5d5fdb09c
AbuzerZulkifly <159270297+AbuzerZulkifly@users.noreply.github.com>
2025-12-27 18:24:34 +0530

    Filter Section Done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
     <div className='min-h-screen bg-background text-foreground overflow-x-hidden'>
       <StarBackground />
       <Navbar 
-       brandName="StarTech"
+       brandName="Abuzer Zulkifly"
       />
       <main>
         <HeroSection
